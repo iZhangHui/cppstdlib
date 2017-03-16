85bb3f5996246d733e22684d319d7c27096d4bd6
Henry Zhang <zhanghui629@gmail.com>
2017-03-16 10:38:13 +0800

    test heap alg

--- algo/heap1.cpp
+++ algo/heap1.cpp
@@ -9,7 +9,17 @@
  * warranty, and with no claim as to its suitability for any purpose.
  */
 #include "algostuff.hpp"
+#include <functional>
 using namespace std;
+
+// template< class RandomIt >
+// void push_heap( RandomIt first, RandomIt last ); (1)
+// template< class RandomIt, class Compare >
+// void push_heap( RandomIt first, RandomIt last, Compare comp ); (2)
+// Inserts the element at the position last-1 into the max heap
+// defined by the range [first, last-1). The first version of the
+// function uses operator< to compare the elements, the second uses
+// the given comparison function comp.
 
 int main()
 {
@@ -19,28 +29,59 @@ int main()
     INSERT_ELEMENTS(coll,5,9);
     INSERT_ELEMENTS(coll,1,4);
 
-    PRINT_ELEMENTS (coll, "on entry:           ");
+    PRINT_ELEMENTS(coll, "on entry:           ");
 
     // convert collection into a heap
-    make_heap (coll.begin(), coll.end());
+    make_heap(coll.begin(), coll.end());
 
-    PRINT_ELEMENTS (coll, "after make_heap():  ");
+    // make smallest heap
+    // make_heap(coll.begin(), coll.end(), greater<int>());
+
+    PRINT_ELEMENTS(coll, "after make_heap():  ");
 
     // pop next element out of the heap
-    pop_heap (coll.begin(), coll.end());
+    pop_heap(coll.begin(), coll.end());
     coll.pop_back();
 
     PRINT_ELEMENTS (coll, "after pop_heap():   ");
 
     // push new element into the heap
-    coll.push_back (17);
-    push_heap (coll.begin(), coll.end());
+    coll.push_back(17);
+    push_heap(coll.begin(), coll.end());
 
     PRINT_ELEMENTS (coll, "after push_heap():  ");
 
     // convert heap into a sorted collection
     // - NOTE: after the call it is no longer a heap
-    sort_heap (coll.begin(), coll.end());
+    sort_heap(coll.begin(), coll.end());
 
     PRINT_ELEMENTS (coll, "after sort_heap():  ");
 }
+
+
+// int main()
+// {
+//     std::vector<int> v { 3, 1, 4, 1, 5, 9 };
+
+//     std::cout << "initially, v: ";
+//     for (auto i : v)
+//         std::cout << i << ' ';
+//     std::cout << '\n';
+
+//     std::make_heap(v.begin(), v.end());
+
+//     std::cout << "after make_heap, v: ";
+//     for (auto i : v)
+//         std::cout << i << ' ';
+//     std::cout << '\n';
+
+//     std::pop_heap(v.begin(), v.end());
+//     auto largest = v.back();
+//     v.pop_back();
+//     std::cout << "largest element: " << largest << '\n';
+
+//     std::cout << "after removing the largest element, v: ";
+//     for (auto i : v)
+//         std::cout << i << ' ';
+//     std::cout << '\n';
+// }
