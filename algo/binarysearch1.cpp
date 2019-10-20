a85658f4a3e7257bf044585eb9f6e0b29045972b
Henry Zhang <zhanghui629@gmail.com>
2019-10-20 19:42:12 +0800

    format binarysearch1.cpp

--- algo/binarysearch1.cpp
+++ algo/binarysearch1.cpp
@@ -15,22 +15,20 @@ int main()
 {
     list<int> coll;
 
-    INSERT_ELEMENTS(coll,1,9);
+    INSERT_ELEMENTS(coll, 1, 9);
     PRINT_ELEMENTS(coll);
 
     // check existence of element with value 5
     if (binary_search(coll.cbegin(), coll.cend(), 5)) {
         cout << "5 is present" << endl;
-    }
-    else {
+    } else {
         cout << "5 is not present" << endl;
     }
 
     // check existence of element with value 42
     if (binary_search(coll.cbegin(), coll.cend(), 42)) {
         cout << "42 is present" << endl;
-    }
-    else {
+    } else {
         cout << "42 is not present" << endl;
     }
 }
