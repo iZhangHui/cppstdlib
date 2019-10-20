6db6c0910f3aa51685fba12173ad0be67b5dd67a
Henry Zhang <zhanghui629@gmail.com>
2019-10-20 21:07:04 +0800

    test nth_element

--- algo/nthelement1.cpp
+++ algo/nthelement1.cpp
@@ -14,39 +14,43 @@ using namespace std;
 int main()
 {
     deque<int> coll;
+    // vector<int> coll;
 
-    INSERT_ELEMENTS(coll,3,7);
-    INSERT_ELEMENTS(coll,2,6);
-    INSERT_ELEMENTS(coll,1,5);
+    INSERT_ELEMENTS(coll, 3, 7);
+    INSERT_ELEMENTS(coll, 2, 6);
+    INSERT_ELEMENTS(coll, 1, 5);
     PRINT_ELEMENTS(coll);
 
     // extract the four lowest elements
-    nth_element (coll.begin(),     // beginning of range
-                 coll.begin()+3,   // element that should be sorted correctly
-                 coll.end());      // end of range
+    nth_element(coll.begin(),     // beginning of range
+                coll.begin()+3,   // element that should be sorted correctly
+                coll.end());      // end of range
+    PRINT_ELEMENTS(coll);
 
     // print them
     cout << "the four lowest elements are:  ";
-    copy (coll.cbegin(), coll.cbegin()+4,
-          ostream_iterator<int>(cout," "));
+    copy(coll.cbegin(), coll.cbegin()+4,
+         ostream_iterator<int>(cout," "));
     cout << endl;
 
     // extract the four highest elements
-    nth_element (coll.begin(),     // beginning of range
-                 coll.end()-4,     // element that should be sorted correctly
-                 coll.end());      // end of range
+    nth_element(coll.begin(),     // beginning of range
+                coll.end()-4,     // element that should be sorted correctly
+                coll.end());      // end of range
+    PRINT_ELEMENTS(coll);
 
     // print them
     cout << "the four highest elements are: ";
-    copy (coll.cend()-4, coll.cend(),
-          ostream_iterator<int>(cout," "));
+    copy(coll.cend()-4, coll.cend(),
+         ostream_iterator<int>(cout," "));
     cout << endl;
 
     // extract the four highest elements (second version)
-    nth_element (coll.begin(),     // beginning of range
-                 coll.begin()+3,   // element that should be sorted correctly
-                 coll.end(),       // end of range
-                 greater<int>());  // sorting criterion
+    nth_element(coll.begin(),     // beginning of range
+                coll.begin()+3,   // element that should be sorted correctly
+                coll.end(),       // end of range
+                greater<int>());  // sorting criterion
+    PRINT_ELEMENTS(coll);
 
     // print them
     cout << "the four highest elements are: ";
